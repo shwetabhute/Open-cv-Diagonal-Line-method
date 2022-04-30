50599c9740750f223f0b26f277f1884c0688b4d4
shwetabhute <99078584+shwetabhute@users.noreply.github.com>
2022-04-30 12:57:30 +0530

    Update s7.py

--- s7.py
+++ s7.py
@@ -1,12 +1,25 @@
+# importing cv2
 import cv2
+# path 
 path = 'numpy.jpg'
+# Reading an image in default mode
 image = cv2.imread(path)
+# Window name in which image is displayed
 window_name = 'Image'
+# Start coordinate, here (0, 0)
+# represents the top left corner of image
 start_point = (1, 1)
+# End coordinate, here (250, 250)
+# represents the bottom right corner of image
 end_point = (300, 165)
+# Green color in BGR
 color = (0, 255, 0)
+# Line thickness of 9 px
 thickness = 9
+# Using cv2.line() method
+# Draw a diagonal green line with thickness of 9 px
 image = cv2.line(image, start_point, end_point, color, thickness)
+# Displaying the image 
 cv2.imshow(window_name, image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
